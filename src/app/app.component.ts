36f1cd2b427f82065a5bbb4d11739fc992dcb9cd
irixa <irixa.vales@upr.edu>
2018-07-23 18:35:01 -0400

    changed side bar and added hidden tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, MenuController, Nav } from 'ionic-angular';
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {CalendarComponent} from "../components/calendar/calendar";
+import {DayScheduleComponent} from "../components/day-schedule/day-schedule";
+import {WeekScheduleComponent} from "../components/week-schedule/week-schedule";
+import {IndividualCourseComponent} from "../components/individual-course/individual-course";
 
 
 @Component({
@@ -17,7 +20,9 @@ export class MyApp {
 
   // make HelloIonicPage the root (or first) page
   rootPage = HelloIonicPage;
-  pages: Array<{title: string, component: any}>;
+  dividers: Array<{title: string, open: Boolean, pages: Array<any>}>;
+  course_pages: Array<{title: string, component: any}>;
+  schedule_pages: Array<{title: string, component: any}>;
 
   constructor(
     public platform: Platform,
@@ -28,9 +33,25 @@ export class MyApp {
     this.initializeApp();
 
     // set our app's pages
-    this.pages = [
-      { title: 'Hello Ionic', component: HelloIonicPage },
-      { title: 'My First List', component: ListPage }
+    this.schedule_pages = [
+      { title: 'Today', component: DayScheduleComponent },
+      { title: 'This Week', component: WeekScheduleComponent },
+      { title: 'Calendar', component: CalendarComponent }
+    ];
+
+    this.course_pages = [
+      { title: 'this is hardcoded', component: IndividualCourseComponent },
+      { title: 'Mate', component: HelloIonicPage },
+      { title: 'Icom', component: HelloIonicPage },
+      { title: 'Quim', component: HelloIonicPage },
+      { title: 'Inge', component: HelloIonicPage }
+    ];
+
+    // dividers in side menu
+    this.dividers = [
+      { title: 'Dashboard', open: false, pages: this.schedule_pages },
+      { title: 'Courses', open: false, pages: this.course_pages },
+      { title: 'Schedule', open: false, pages: this.schedule_pages }
     ];
   }
 
@@ -43,7 +64,16 @@ export class MyApp {
     });
   }
 
+  openDivider(divider) {
+    // close other dividers
+    this.dividers.map(divider => divider.open = false);
+    // open selected divider
+    divider.open = true;
+  }
+
   openPage(page) {
+    // close all dividers
+    this.dividers.map(divider => divider.open = false);
     // close the menu when clicking a link from the menu
     this.menu.close();
     // navigate to the new page if it is not the current page
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
+import { MbscModule } from '@mobiscroll/angular';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { RouterModule } from "@angular/router";
 
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
@@ -10,28 +13,49 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import {CalendarComponent} from "../components/calendar/calendar";
+import {DayScheduleComponent} from "../components/day-schedule/day-schedule";
+import {WeekScheduleComponent} from "../components/week-schedule/week-schedule";
+import {IndividualCourseComponent} from "../components/individual-course/individual-course";
+import { TaskProvider } from '../providers/task/task';
+
 @NgModule({
   declarations: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    CalendarComponent,
+    DayScheduleComponent,
+    WeekScheduleComponent,
+    IndividualCourseComponent
   ],
-  imports: [
+  imports: [ 
+    MbscModule,
+    FormsModule,
     BrowserModule,
+    RouterModule,
     IonicModule.forRoot(MyApp),
+  ],
+  exports: [
+    RouterModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HelloIonicPage,
     ItemDetailsPage,
-    ListPage
+    ListPage,
+    CalendarComponent,
+    DayScheduleComponent,
+    WeekScheduleComponent,
+    IndividualCourseComponent
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    TaskProvider
   ]
 })
 export class AppModule {}
